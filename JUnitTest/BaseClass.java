d2582418bbf7b6fb12e6a136af6dc9c06a2303c8
PrativaKafley <40444837+PrativaK@users.noreply.github.com>
2020-01-20 15:25:29 -0600

    Update BaseClass.java

--- JUnitTest/BaseClass.java
+++ JUnitTest/BaseClass.java
@@ -3,6 +3,7 @@ package TestPkg;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.net.URL;
 import java.io.*;
 
 import org.junit.AfterClass;
@@ -19,26 +20,35 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 public class BaseClass {
 
 	public static WebDriver driver;
+	public static final String IE_DRIVER = "Resource//IEDriverServer.exe";
+	public static final String LogFile = "Resource//A.txt";
 	public static WebDriver getDriver() {
 		return driver;		
 	}
 	
 	@BeforeClass
 	public static void beforeSuite() {
+		
 		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
 		
 		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 		
 		capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
 		
+		ClassLoader classloader = BaseClass.class.getClassLoader();
+		
 		InternetExplorerDriverService service = new InternetExplorerDriverService.Builder()
 												//Add path of IEDriver from the local machine where builds is running
-												.usingDriverExecutable(new File("C:\\Users\\itspr\\Desktop\\IEDriverServer_Win32_3.150.1\\IEDriverServer.exe"))
+												.usingDriverExecutable(new File(classloader.getResource(IE_DRIVER).getFile()))
 												.usingAnyFreePort()
 												.build();
 		InternetExplorerOptions options = new InternetExplorerOptions();
 		options.merge(capabilities);
 		driver = new InternetExplorerDriver(service, options);
+		
+		
+
+		
 		
 	}
 	
@@ -51,27 +61,25 @@ public class BaseClass {
 		}
 	}
 	
+	
+	
 	@BeforeClass
      public static void OutComeTest() {
-		 // Creating a File object that represents the disk file. 
+	
+		ClassLoader logFileLoad = BaseClass.class.getClassLoader();
+		File file = new File(logFileLoad.getResource(LogFile).getFile());
+		String absolutePath = file.getAbsolutePath(); 
 	        PrintStream o = System.out;
 			try {
-				o = new PrintStream(new File("A.txt"));
+				o = new PrintStream(absolutePath);
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} 
 	  
-	        // Store current System.out before assigning a new value 
-	       // PrintStream console = System.out; 
-	  
 	        // Assign o to output stream 
 	        System.setOut(o); 
-	        //System.out.println("This will be written to the text file"); 
-	  
-	        // Use stored value for output stream 
-	       // System.setOut(console); 
-	       // System.out.println("This will be written on the console!"); 
+		
 	}
 	
 	@Test
